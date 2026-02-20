a318ada1e9d9d0d1bc0b5460045bfe8095cbafa5
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-02-20 19:13:30 +0100

    Creada la clase DtoEspectaculo.java para simplificar el envío de información al frontend. Esta clase agrupa únicamente el ID, artista, fecha y el nombre del escenario como texto, evitando exponer la entidad completa de la base de datos y optimizando la respuesta JSON.

---
+++ src/main/java/edu/esi/ds/esientradas/dto/DtoEspectaculo.java
@@ -0,0 +1,43 @@
+package edu.esi.ds.esientradas.dto;
+
+import java.time.LocalDateTime;
+
+public class DtoEspectaculo {
+
+    private String artista;
+    private LocalDateTime fecha;
+    private String escenario;
+    private Long id;
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public void setEscenario(String nombre) {
+        this.escenario = nombre;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public String getEscenario() {
+        return escenario;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
