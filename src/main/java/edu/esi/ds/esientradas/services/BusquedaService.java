44d792cf9f21785412d27d1d3a4ed218c32d3f3c
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-02-20 19:06:45 +0100

    Actualizada la clase BusquedaService.java para centralizar toda la lógica de consultas del sistema. Se han inyectado dos nuevos repositorios, EspectaculoDao y EntradaDao.
    He implementado el método getEspectaculos(artista) para filtrar eventos por nombre de artista y, getEntradas(espectaculoId). Este último método recibe el identificador como texto desde el controlador, lo convierte a formato numérico (Long) y utiliza el nuevo EntradaDao para obtener la lista de tickets asociados al espectáculo desde la base de datos.

--- src/main/java/edu/esi/ds/esientradas/services/BusquedaService.java
+++ src/main/java/edu/esi/ds/esientradas/services/BusquedaService.java
@@ -5,17 +5,37 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import edu.esi.ds.esientradas.model.Escenario;
+import edu.esi.ds.esientradas.model.Espectaculo;
+import edu.esi.ds.esientradas.model.Entrada;
 import edu.esi.ds.esientradas.dao.EscenarioDao;
+import edu.esi.ds.esientradas.dao.EscpectaculoDao;
+import edu.esi.ds.esientradas.dao.EntradaDao;
 
 @Service
 public class BusquedaService {
 
     @Autowired
     private EscenarioDao escenarioDao;
+    
+    @Autowired
+    private EscpectaculoDao espectaculoDao;
+
+    @Autowired
+    private EntradaDao entradaDao;
+
+    public List<Entrada> getEntradas(String espectaculoId) {
+        // aqui se haria la logica para obtener las entradas de la base de datos
+        return this.entradaDao.findByEspectaculoId(Long.parseLong(espectaculoId)); // se devuelve la lista de entradas obtenida del DAO
+    }
 
     public List<Escenario> getEscenarios() {
         // aqui se haria la logica para obtener los escenarios de la base de datos
         return this.escenarioDao.findAll(); // se devuelve la lista de escenarios obtenida del DAO
+    }
+
+    public List<Espectaculo> getEspectaculos(String artista) {
+        // aqui se haria la logica para obtener los espectaculos de la base de datos
+        return this.espectaculoDao.findByArtista(artista); // se devuelve la lista de espectaculos obtenida del DAO
     }
 
 
