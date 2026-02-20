9c795b1a28ee73ee8047828c7c8eb517c236e7bf
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-02-20 19:08:57 +0100

    Actualizado EscenarioController.java, se ha inyectado el servicio EscenariosService mediante @Autowired para delegar en él la persistencia de los datos.
    Además, he implementado una capa de validación previa en el endpoint /insertar: ahora el sistema verifica si el nombre o la descripción del escenario llegan vacíos o nulos antes de intentar guardarlos. En caso de que los datos estén incompletos, el controlador lanza una ResponseStatusException con un código 400 (Bad Request).

--- src/main/java/edu/esi/ds/esientradas/http/EscenarioController.java
+++ src/main/java/edu/esi/ds/esientradas/http/EscenarioController.java
@@ -2,15 +2,28 @@ package edu.esi.ds.esientradas.http;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import edu.esi.ds.esientradas.model.Escenario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
+import edu.esi.ds.esientradas.services.EscenariosService;
 
 @RestController
 @RequestMapping("/escenarios")
 public class EscenarioController {
 
+    @Autowired
+    private EscenariosService escenariosService;
+
     @RequestMapping("/insertar")
     public void insertarEscenario(@RequestBody Escenario escenario) {
+        if (escenario.getNombre() == null || escenario.getNombre().isEmpty() || escenario.getDescripcion() == null
+                || escenario.getDescripcion().isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El nombre y la descripción no pueden ser nulos");
+        }
+        this.escenariosService.insertarEscenario(escenario);
+
         System.out.println(escenario.getNombre());
         System.err.println(escenario.getDescripcion());
     }
