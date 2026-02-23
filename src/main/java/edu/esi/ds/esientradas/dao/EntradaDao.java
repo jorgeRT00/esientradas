e035e531da9d396d3c1815cdb4fa69c8c913146a
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-02-23 19:42:46 +0100

    Actualizado el método updateEstado para que el parámetro :estado utilice el tipo Estado (Enum) en lugar de una cadena de texto. Este cambio corrige errores de desajuste de tipos en las consultas JPQL y asegura que el valor se persista correctamente según la definición de la entidad.

--- src/main/java/edu/esi/ds/esientradas/dao/EntradaDao.java
+++ src/main/java/edu/esi/ds/esientradas/dao/EntradaDao.java
@@ -2,10 +2,18 @@ package edu.esi.ds.esientradas.dao;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import edu.esi.ds.esientradas.model.Entrada;
+import edu.esi.ds.esientradas.model.Estado;
 
 public interface EntradaDao extends JpaRepository<Entrada, Long> {
     
-    // Spring Data JPA creará la consulta SQL automáticamente al ver este nombre
     List<Entrada> findByEspectaculoId(Long espectaculoId);
+
+    @Query(value = "UPDATE Entrada e SET e.estado = :estado WHERE e.id = :entradaId")
+    @Modifying
+    void updateEstado(@Param("entradaId") Long entradaId, @Param("estado") Estado estado);
 }
