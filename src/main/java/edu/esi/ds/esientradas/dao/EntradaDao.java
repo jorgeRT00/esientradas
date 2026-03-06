113d967ba01eeabcacf426dde5ad2b85c38b455d
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-03-06 19:41:37 +0100

    Implementación del DTO en la persistencia: Ahora la consulta @Query no devuelve una lista de objetos pesados, sino que instancia directamente el objeto DtoEntradas. Lógica de Agregación: Ha añadido una consulta JPQL que realiza cuatro operaciones en una sola pasada por la tabla entrada: COUNT(e): Calcula el aforo total. SUM(CASE...): Clasifica y suma las entradas según su Estado (DISPONIBLE, VENDIDA, RESERVADA). Optimización: Al usar WHERE e.espectaculo.id = :espectaculoId, la consulta es extremadamente rápida porque solo analiza las entradas del artista seleccionado.

--- src/main/java/edu/esi/ds/esientradas/dao/EntradaDao.java
+++ src/main/java/edu/esi/ds/esientradas/dao/EntradaDao.java
@@ -5,15 +5,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+import edu.esi.ds.esientradas.dto.DtoEntradas;
 import edu.esi.ds.esientradas.model.Entrada;
 import edu.esi.ds.esientradas.model.Estado;
 
 public interface EntradaDao extends JpaRepository<Entrada, Long> {
-    
+
     List<Entrada> findByEspectaculoId(Long espectaculoId);
 
     @Query(value = "UPDATE Entrada e SET e.estado = :estado WHERE e.id = :entradaId") // Consulta JPQL para actualizar el estado de una entrada
     @Modifying
     void updateEstado(@Param("entradaId") Long entradaId, @Param("estado") Estado estado); // Método para actualizar el estado de una entrada
+
+    @Query("""
+        SELECT new edu.esi.ds.esientradas.dto.DtoEntradas(
+            COUNT(e), 
+            SUM(CASE WHEN e.estado = edu.esi.ds.esientradas.model.Estado.DISPONIBLE THEN 1L ELSE 0L END),
+            SUM(CASE WHEN e.estado = edu.esi.ds.esientradas.model.Estado.VENDIDA THEN 1L ELSE 0L END),
+            SUM(CASE WHEN e.estado = edu.esi.ds.esientradas.model.Estado.RESERVADA THEN 1L ELSE 0L END)
+        )
+        FROM Entrada e
+        WHERE e.espectaculo.id = :espectaculoId
+    """)
+    DtoEntradas getNumeroEntradasDT(@Param("espectaculoId") Long espectaculoId);
 }
