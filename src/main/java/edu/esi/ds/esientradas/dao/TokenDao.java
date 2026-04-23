0e6bb37fecdc4c31f278f3ea7de353db408c4f80
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-24 00:20:27 +0200

    Corregido TokenDao para usar Token en vez de Entrada

--- src/main/java/edu/esi/ds/esientradas/dao/TokenDao.java
+++ src/main/java/edu/esi/ds/esientradas/dao/TokenDao.java
@@ -1,11 +1,11 @@
 package edu.esi.ds.esientradas.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import edu.esi.ds.esientradas.model.Token;
+import java.util.Optional;
 
+public interface TokenDao extends JpaRepository<Token, String> {
 
-import edu.esi.ds.esientradas.model.Entrada;
-
-
-public interface TokenDao extends JpaRepository<Entrada, Long> {
+    Optional<Token> findByValor(String valor);
 
 }
