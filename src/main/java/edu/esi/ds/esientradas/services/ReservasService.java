bfca966084a0cb798956d2f2435eb68dc35dba06
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-24 00:20:51 +0200

    Implementado flujo de compra: guarda token, verifica usuario y marca entrada como VENDIDA

--- src/main/java/edu/esi/ds/esientradas/services/ReservasService.java
+++ src/main/java/edu/esi/ds/esientradas/services/ReservasService.java
@@ -9,9 +9,16 @@ import edu.esi.ds.esientradas.model.Entrada;
 import edu.esi.ds.esientradas.model.Estado;
 import jakarta.transaction.Transactional;
 import edu.esi.ds.esientradas.model.Token;
+import edu.esi.ds.esientradas.dao.TokenDao;
+
 
 @Service
 public class ReservasService {
+
+    @Autowired
+    private UsuariosService usuariosService;
+    @Autowired
+    private TokenDao tokenDao;
 
     @Autowired
     private EntradaDao entradaDao;
@@ -24,18 +31,41 @@ public class ReservasService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Entrada no disponible.");
         }
 
-        // 1. Crear el token con el ID real (sin comillas)
+        // 1. Crear el token y asociarlo a la entrada y a la sesión
         Token token = new Token();
         token.setEntrada(entrada);
         token.setSession(sessionId); // Usamos el parámetro, no el texto "sessionId"
 
         // 2. IMPORTANTE: Guardar el token explícitamente
-        // this.tokenDao.save(token);
-        this.entradaDao.save(entrada); // Guardamos la entrada para generar el ID del token
+        this.tokenDao.save(token);
 
         // 3. Actualizar la entrada
         this.entradaDao.updateEstado(entradaId, Estado.RESERVADA);
 
         return entrada.getPrecio();
+    }
+
+    @Transactional
+    public String comprar(String tokenEntrada, String tokenUsuario, String sessionId) {
+        
+        // 1º Veririficamos que el token de usuario llamando a esiurusuarios
+        String emailUsuario = this.usuariosService.checkToken(tokenUsuario);
+        if(emailUsuario == null){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token de usuario no válido.");
+        }
+
+        // 2º Buscar el token de la entrada en la base de datos
+        Token token = this.tokenDao.findById(tokenEntrada).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Token de entrada no encontrado."));
+
+        // 3º Verificar que el token de la entrada corresponde a la sesión actual
+        if (!token.getSession().equals(sessionId)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token de entrada no válido para esta sesión.");
+        }
+
+        // 4º Marcar la entrada como vendida
+        Entrada entrada = token.getEntrada();
+        this.entradaDao.updateEstado(entrada.getId(), Estado.VENDIDA);
+
+        return "Comprar realizada con éxito para el usuario: " + emailUsuario;
     }
 }
