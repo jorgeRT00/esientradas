8690fd884d004f9dbb53f72923c785edafc7e43d
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-24 00:37:22 +0200

    Añadido @EnableScheduling para activar tareas programadas

--- src/main/java/edu/esi/ds/esientradas/EsientradasApplication.java
+++ src/main/java/edu/esi/ds/esientradas/EsientradasApplication.java
@@ -2,10 +2,11 @@ package edu.esi.ds.esientradas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EsientradasApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EsientradasApplication.class, args);
 	}
