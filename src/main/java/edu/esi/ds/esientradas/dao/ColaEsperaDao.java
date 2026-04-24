a40effd2847d7421e76805582444eb579b448003
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-24 13:32:40 +0200

    he implementado el repositorio ColaEsperaDao con búsqueda por espectáculo, usuario y conteo de posiciones

---
+++ src/main/java/edu/esi/ds/esientradas/dao/ColaEsperaDao.java
@@ -0,0 +1,21 @@
+package edu.esi.ds.esientradas.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import edu.esi.ds.esientradas.model.ColaEspera;
+import edu.esi.ds.esientradas.model.Espectaculo;
+import java.util.List;
+import java.util.Optional;
+
+public interface ColaEsperaDao extends JpaRepository<ColaEspera, Long> {
+
+    // Todos los uusuraios en la cola de espera de un espectaculo ordenados por su
+    // posicion
+    List<ColaEspera> findByEspectaculoOrderByPosicionAsc(Espectaculo espectaculo);
+
+    // Bucar si un usuario esta en la cola de espera de un espectaculo
+    Optional<ColaEspera> findByEspectaculoAndEmailUsuario(Espectaculo espectaculo, String emailUsuario);
+
+    // Contar cuantos hay delanete de un usuario.
+    int countByEspectaculoAndPosicionLessThan(Espectaculo espectaculo, int posicion);
+
+}
