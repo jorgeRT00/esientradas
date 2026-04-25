4aa8f8de7039d47b56faacf6d53e02ec08ef3550
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-25 03:12:43 +0200

    Añado método nativo para eliminar tokens y evitar caché de JPA

--- src/main/java/edu/esi/ds/esientradas/dao/TokenDao.java
+++ src/main/java/edu/esi/ds/esientradas/dao/TokenDao.java
@@ -1,11 +1,19 @@
 package edu.esi.ds.esientradas.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import edu.esi.ds.esientradas.model.Token;
 import java.util.Optional;
 
 public interface TokenDao extends JpaRepository<Token, String> {
 
     Optional<Token> findByValor(String valor);
+
+    @Modifying // Indica que este método realiza una operación de modificación (DELETE)
+    @Query(value = "DELETE FROM token WHERE valor = :valor", nativeQuery = true) // Consulta SQL nativa para eliminar un token por su valor
+    void deleteByValorNativo(@Param("valor") String valor); // Parámetro para el valor del token a eliminar
 
 }
