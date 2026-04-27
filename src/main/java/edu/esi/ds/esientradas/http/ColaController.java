838c3c2560bebc1c65b9a0f2fc6dd96de86fc6dd
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-04-27 12:27:23 +0200

    se añaden los endpoints /turno (GET) y /salir (DELETE), se extrae la validación del token a un método privado para no repetirlo, y se añade @CrossOrigin con todos los métodos necesarios.

--- src/main/java/edu/esi/ds/esientradas/http/ColaController.java
+++ src/main/java/edu/esi/ds/esientradas/http/ColaController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
@@ -11,8 +13,10 @@ import edu.esi.ds.esientradas.services.ColaService;
 import edu.esi.ds.esientradas.services.UsuariosService;
 import org.springframework.http.HttpStatus;
 
+
 @RestController
 @RequestMapping("/cola")
+@CrossOrigin(origins = "http://localhost:4200") // Cambia esto por el origen de tu frontend
 public class ColaController {
 
     @Autowired
@@ -21,22 +25,32 @@ public class ColaController {
     @Autowired
     private UsuariosService usuariosService;
 
-    @PostMapping("/unirse")
-    public String unirse(@RequestParam Long espectaculoId, @RequestParam String tokenUsuario) {
-        String emailUsuario = usuariosService.checkToken(tokenUsuario);
-        if(emailUsuario == null) {
+    private String validarToken(String tokenUsuario) {
+        String email = usuariosService.checkToken(tokenUsuario);
+        if (email == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token de usuario no valido.");
         }
-        return colaService.unirse(espectaculoId, emailUsuario);
+        return email;
+    }
+
+    @PostMapping("/unirse")
+    public String unirse(@RequestParam Long espectaculoId, @RequestParam String tokenUsuario) {
+        return colaService.unirse(espectaculoId, validarToken(tokenUsuario));
     }
 
     @GetMapping("/posicion")
     public int posicion(@RequestParam Long espectaculoId, @RequestParam String tokenUsuario) {
-        String emailUsuario = usuariosService.checkToken(tokenUsuario);
-        if(emailUsuario == null) {
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token de usuario no valido.");
-        }
-        return colaService.consultarPosicion(espectaculoId, emailUsuario);
+        return colaService.consultarPosicion(espectaculoId, validarToken(tokenUsuario));
+    }
+
+    @GetMapping("/turno")
+    public boolean tieneTurno(@RequestParam Long espectaculoId, @RequestParam String tokenUsuario) {
+        return colaService.tieneTurno(espectaculoId, validarToken(tokenUsuario));
+    }
+
+    @DeleteMapping("/salir")
+    public void salir(@RequestParam Long espectaculoId, @RequestParam String tokenUsuario) {
+        colaService.salirDeCola(espectaculoId, validarToken(tokenUsuario));
     }
 
 }
