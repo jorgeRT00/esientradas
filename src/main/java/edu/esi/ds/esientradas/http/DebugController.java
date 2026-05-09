0a3f627afe3e984e257fab21e0f161b557143242
jorge <Jorge.Rodriguez11@alu.uclm.es>
2026-05-09 12:49:09 +0200

    fix: eliminar DebugController
    Exponía información interna de la BD (/debug/entradas-info)
    sin autenticación. No tiene uso en producción.

--- src/main/java/edu/esi/ds/esientradas/http/DebugController.java
+++ src/main/java/edu/esi/ds/esientradas/http/DebugController.java
@@ -1,37 +1,43 @@
-package edu.esi.ds.esientradas.http;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import edu.esi.ds.esientradas.dao.EntradaDao;
-import edu.esi.ds.esientradas.model.Entrada;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-@RestController
-@RequestMapping("/debug")
-public class DebugController {
-    @Autowired
-    private EntradaDao entradaDao;
-
-    @GetMapping("/entradas-info")
-    public Map<String, Object> diagnosticarEntradas() {
-        List<Entrada> todas = entradaDao.findAll();
-        List<String> tipos = entradaDao.obtenerTiposEntrada();
-
-        long butacas = todas.stream().filter(e -> e.getClass().getSimpleName().equals("Precisa")).count();
-        long zonas = todas.stream().filter(e -> e.getClass().getSimpleName().equals("DeZona")).count();
-
-        return Map.of(
-                "total_entradas", todas.size(),
-                "butacas", butacas,
-                "zonas", zonas,
-                "tipos_en_bd", tipos != null ? tipos : List.of("(vacio)"),
-                "tipos_detectados", todas.stream()
-                        .map(e -> e.getClass().getSimpleName())
-                        .distinct()
-                        .collect(Collectors.toList()));
-    }
-}
+/*
+ * package edu.esi.ds.esientradas.http;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.GetMapping;
+ * import org.springframework.web.bind.annotation.RequestMapping;
+ * import org.springframework.web.bind.annotation.RestController;
+ * import edu.esi.ds.esientradas.dao.EntradaDao;
+ * import edu.esi.ds.esientradas.model.Entrada;
+ * import java.util.List;
+ * import java.util.Map;
+ * import java.util.stream.Collectors;
+ * 
+ * @RestController
+ * 
+ * @RequestMapping("/debug")
+ * public class DebugController {
+ * 
+ * @Autowired
+ * private EntradaDao entradaDao;
+ * 
+ * @GetMapping("/entradas-info")
+ * public Map<String, Object> diagnosticarEntradas() {
+ * List<Entrada> todas = entradaDao.findAll();
+ * List<String> tipos = entradaDao.obtenerTiposEntrada();
+ * 
+ * long butacas = todas.stream().filter(e ->
+ * e.getClass().getSimpleName().equals("Precisa")).count();
+ * long zonas = todas.stream().filter(e ->
+ * e.getClass().getSimpleName().equals("DeZona")).count();
+ * 
+ * return Map.of(
+ * "total_entradas", todas.size(),
+ * "butacas", butacas,
+ * "zonas", zonas,
+ * "tipos_en_bd", tipos != null ? tipos : List.of("(vacio)"),
+ * "tipos_detectados", todas.stream()
+ * .map(e -> e.getClass().getSimpleName())
+ * .distinct()
+ * .collect(Collectors.toList()));
+ * }
+ * }
+ */
